29df08a70839ddc51bcb9c4909c3f58ad680040e
Kliment <104510777+Tijeyd@users.noreply.github.com>
2022-11-17 23:33:50 +0300

    ProjectKM

---
+++ src/test/java/org/example/unit/ClassTest.java
@@ -0,0 +1,71 @@
+package org.example.unit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ClassTest {
+
+    //// ЭТО ТЕСТ НЕ ОТПРАВЛЯЙ!!!!!!!!!
+
+
+
+
+
+
+
+    ///
+
+
+
+
+
+
+
+
+
+
+    ///
+
+
+
+
+
+
+
+
+
+
+
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+
+
+
+
+    ////
+
+
+
+
+
+
+
+
+    ////
+
+
+
+
+
+
+
+
+
+
+}
+
+
